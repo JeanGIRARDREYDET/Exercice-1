50d1c97de8bbe3d9628d058f92493a9d104377ee
cogitafac <cogitafac@M1.local>
2022-05-10 13:54:01 +0200

    Exercice 1 GRB Vertical

--- App.js
+++ App.js
@@ -4,8 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.containerG}></Text>
+      <Text style={styles.containerR}></Text>
+      <Text style={styles.containerB}></Text>
       <StatusBar style="auto" />
+
+     
+
     </View>
   );
 }
@@ -13,8 +18,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+   
+  },  
+  containerG: {
+    flex: 2,
+    backgroundColor: '#0F0',
+ paddingLeft:0,
+    width : '100%', 
+  
+   
+  },  containerR: {
+    flex: 1,
+    backgroundColor: '#f00',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width : '100%',   
+    
+  },  containerB: {
+    flex: 3,
+    backgroundColor: '#00f',
+    alignItems: 'center',
+    justifyContent: 'center',
+     width : '100%',
+     
   },
 });
